b86e14495a994b016628b46a2abdea98684dec11
Trylena <ariadnamiino@gmail.com>
2023-05-07 17:41:43 -0300

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { LoginComponent } from './components/login/login.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    EstudiosComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    LoginComponent,
+    SobreMiComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
