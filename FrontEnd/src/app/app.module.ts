4694dcdaf3cef01cef54d36c576e39788f4bb3fe
Trylena <ariadnamiino@gmail.com>
2023-05-10 02:12:26 -0300

    2

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/1header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
-import { BannerComponent } from './components/2banner/banner.component';
-import { EstudiosComponent } from './components/5estudios/estudios.component';
-import { SkillsComponent } from './components/6skills/skills.component';
-import { ExperienciaComponent } from './components/4experiencia/experiencia.component';
-import { ProyectosComponent } from './components/7proyectos/proyectos.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { LoginComponent } from './components/login/login.component';
-import { SobreMiComponent } from './components/3sobre-mi/sobre-mi.component';
-import { FooterComponent } from './components/8footer/footer.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     BannerComponent,
     EstudiosComponent,
     SkillsComponent,
